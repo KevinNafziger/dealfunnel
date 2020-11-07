4b49f67c14a9f84e132bc4aa53adbb0bf7537d3e
Kevin Nafziger <KevinNafziger@gmail.com>
2020-11-06 21:37:42 -0700

    items

---
+++ pages/index.vue
@@ -0,0 +1,125 @@
+<template>
+<div class="home-page">
+
+<section class="intro">
+</section>
+
+<img width=637 height=250
+src="Dfunnels_files/image017.gif" v:shapes="Group_x0020_4507 Rectangle_x0020_510 Rectangle_x0020_512 Rectangle_x0020_514 Picture_x0020_4688 Shape_x0020_590 Picture_x0020_592"><![endif]><!--[if mso & !supportInlineShapes & supportFields]><v:shape
+ id="_x0000_i1025" type="#_x0000_t75" style='width:474.3pt;height:180.45pt'>
+ <v:imagedata croptop="-65520f" cropbottom="65520f"/>
+</v:shape><span style='mso-element:field-end'></span><![endif]--></p>
+
+<p class=MsoNormal style='margin-top:0in;margin-right:0in;margin-bottom:0in;
+margin-left:-.25pt;text-indent:-.5pt;line-height:97%'><span style='font-size:
+20.0pt;mso-bidi-font-size:11.0pt;line-height:97%'>Focus – <span class=SpellE>DealFunnel</span>
+is initially focused on delivering information on emerging technology
+businesses serving the financial services industry, but we <span class=GramE>don’t</span>
+stop there. In addition to our coverage of Fintech, <span class=SpellE>InsurTech</span>,
+and <span class=SpellE>WealthTech</span> firms, we are also compiling a glut of
+information on Blockchain, DLT and Crypto firms as well as info on the latest
+companies in <span class=SpellE>HealthTech</span>, Artificial Intelligence and
+LegalTech, just to name a few.<span style='mso-spacerun:yes'>  </span></span></p>
+
+<h1 style='margin-left:0in'>Our Readers<u style='text-underline:black'><span
+style='text-underline:black;text-decoration:none;text-underline:none'><span
+style='mso-spacerun:yes'>   </span></span></u></h1>
+
+<p class=MsoNormal style='margin-top:0in;margin-right:167.4pt;margin-bottom:
+4.75pt;margin-left:-.25pt;text-indent:-.5pt;line-height:109%'><!--[if gte vml 1]><v:shape
+ id="Picture_x0020_732" o:spid="_x0000_s1026" type="#_x0000_t75" style='position:absolute;
+ left:0;text-align:left;margin-left:204pt;margin-top:-48.45pt;width:263.5pt;
+ height:648.9pt;z-index:251662336;visibility:visible;mso-wrap-style:square;
+ mso-wrap-distance-left:9pt;mso-wrap-distance-top:0;mso-wrap-distance-right:9pt;
+ mso-wrap-distance-bottom:0;mso-position-horizontal:absolute;
+ mso-position-horizontal-relative:text;mso-position-vertical:absolute;
+ mso-position-vertical-relative:text' o:allowoverlap="f">
+ <v:imagedata src="Dfunnels_files/image018.png" o:title=""/>
+ <w:wrap type="square"/>
+</v:shape><![endif]--><![if !vml]><img width=351 height=866
+src="Dfunnels_files/image019.gif" align=left hspace=12 v:shapes="Picture_x0020_732"><![endif]><span
+class=SpellE><span style='font-size:22.0pt;mso-bidi-font-size:11.0pt;
+line-height:109%'>DealFunnel</span></span><span style='font-size:22.0pt;
+mso-bidi-font-size:11.0pt;line-height:109%'> is a must have for investment
+bankers, venture capitalists, private equity directors, attorneys and company
+executives.<span style='mso-spacerun:yes'>  </span>Not only will <span
+class=SpellE>DealFunnel</span> keep you ahead of the curve, <span class=GramE>our
+products</span> will also help you<span style='mso-spacerun:yes'>  </span>and
+your team streamline and possibly <span class=SpellE>elimiate</span> many tasks
+completed by research teams, including analysts and interns. <span
+class=SpellE>DealFunnels</span> can also be used to send leads<span
+style='mso-spacerun:yes'>  </span>to clients in addition to its use in-house.
+Our product is simple to use and <span class=SpellE>and</span> organized in a
+straightforward manner. </span></p>
+
+<p class=MsoNormal style='margin-bottom:6.2pt'><span style='font-size:22.0pt;
+mso-bidi-font-size:11.0pt;line-height:107%'><span
+style='mso-spacerun:yes'> </span></span></p>
+
+<p class=MsoNormal style='margin-top:0in;margin-right:190.7pt;margin-bottom:
+0in;margin-left:-.25pt;text-indent:-.5pt;line-height:109%'><span
+style='font-size:22.0pt;mso-bidi-font-size:11.0pt;line-height:109%'>To learn
+more about <span class=SpellE>subsciptions</span> and<span
+style='mso-spacerun:yes'>   </span>our services, or to receive a demo, click
+here<span style='mso-spacerun:yes'>  </span>or call 480-438-5302 for more
+information.</span><span style='font-size:36.0pt;mso-bidi-font-size:11.0pt;
+line-height:109%;color:#000099'><span
+style='mso-spacerun:yes'>                     </span></span><span
+style='font-size:22.0pt;mso-bidi-font-size:11.0pt;line-height:109%'><span
+style='mso-spacerun:yes'> </span></span></p>
+
+</div>
+</template>
+
+
+
+<script>
+
+
+export default {
+  
+}
+
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-image: url('~assets/images/dealfunnel.png');
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
