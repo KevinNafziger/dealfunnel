5fba4434bb2e8e72efeb34cced3b549846473c69
celfonica <servicioscelfonica@gmail.com>
2020-11-17 16:21:28 -0700

    Builder / work in progress

---
+++ components/Draggable/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    :id="id"
+    class="card"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @dragover.stop
+  >
+    <slot/>
+  </div>
+</template>
+
+
+<script>
+ export default {
+    props: ['id', 'draggable'],
+    methods: {
+      dragStart: e => {
+        const target = e.target;
+
+        e.dataTransfer.setData('card_id', target.id);
+//if we want cards to not disapear when hold comment line 23 24 25
+      //  setTimeout (() => {
+    //      target.style.display = "none";
+    //    }, 0);
+      }
+    }
+ }
+</script>
