c16e5781aaca128f92ebb3b0033864ad46af64c5
Kevin Nafziger <KevinNafziger@gmail.com>
2020-12-03 10:36:12 -0700

    raises

--- store/raise.js
+++ store/raise.js
@@ -146,7 +146,7 @@ export const mutations = {
 
        if (!this.lendRaisesFetched)
        {  
-            commit("setLend", .data);
+            commit("setLend", data);
         }
        else
        {
