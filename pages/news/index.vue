a4433661b8ffba05247181938411fe1d0ef6263c
Kevin Nafziger <KevinNafziger@gmail.com>
2020-12-03 13:09:03 -0700

    news

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,6 +1,8 @@
 <template>
 <div width="100%" class="w-container-2">
-<div data-v-69296181="" id="top" class="sectionix"><div data-v-69296181="" class="title"><div data-v-69296181="" class="content"><br data-v-69296181=""> <h2 data-v-69296181="">News i style="font-size: 13px; text-align:right; margin-left:3px;">{{ filterMessage }}</i></h2></div></div></div>
+<div data-v-69296181="" id="top" class="sectionix"><div data-v-69296181="" class="title"><div data-v-69296181="" class="content"><br data-v-69296181=""> <h2 data-v-69296181="">News <i style="font-size: 13px; text-align:right; margin-left:3px;">{{ filterMessage }}</i></h2></div></div></div>
+
+<NewsTempSearch></NewsTempSearch>
     <div class="section-15">
       <div class="row-15 w-row">
           <div class="w-col w-col-9">
