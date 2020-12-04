3ff1fdaff8e0639a7d1dc7292737aeabb11a6e1f
Kevin Nafziger <KevinNafziger@gmail.com>
2020-12-04 01:52:49 -0700

    companies

---
+++ pages/companies/_id/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <transition appear>
+    <div v-if="building" class="company__descript_indicator" :style="indicatorStyle">
+      <svg viewBox="0 0 96 72" version="1" xmlns="http://www.w3.org/2000/svg">
+       {{ company.name }}
+       {{company.description}}
+      </svg>
+      {{ progress }}%
+    </div>
+  </transition>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+export default {
+  name: 'Company',
+  data () {
+    return {
+
+      building: true,
+      progress: 0,
+    }
+  },
+
+  computed: {
+
+    .mapState({
+    
+          startingCompanies: state => state.company.us
+          activeInfo: state =>  state.company.activeInfo,
+          firstLoad: state => state.company.firstLoad,
+   }),
+
+    options: () => 
+    ({"position":"bottom-right","backgroundColor":"#2E495E","color":"#00C48D"}),
+
+    indicatorStyle () {
+      const [d1, d2] = this.options.position.split('-')
+      return {
+        [d1]: '20px',
+        [d2]: '20px',
+        'background-color': 'white',
+        color: 'black'
+      }
+
+    },
+
+    companies() {
+
+        if (this.firstLoad == true) {
+
+          return this.startingCompanies
+        }
+
+        else {
+
+          return this.activeInfo;
+        
+        }
+
+     },
+
+    company() {
+
+        return this.companies.find(c => c.id == this.$route.params.id)
+    },
+
+  },
+    
+}
+</script>
+
+<style scoped>
+.company__descript_indicator {
+  box-sizing: border-box;
+  position: fixed;
+  font-family: monospace;
+  padding: 5px 10px;
+  border-radius: 5px;
+  box-shadow: 1px 1px 2px 0px rgba(0,0,0,0.2);
+  width: 88px;
+  z-index: 2147483647;
+  font-size: 16px;
+  line-height: 1.2rem;
+}
+.v-enter-active, .v-leave-active {
+  transition-delay: 0.2s;
+  transition-property: all;
+  transition-duration: 0.3s;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+  width: 1.1em;
+  height: 0.825em;
+  position: relative;
+  top: 1px;
+}
+</style>
---
+++ store/company.js
@@ -0,0 +1,282 @@
+export const state = () => ({
+
+  insurtech: [],
+  blockchain: [],
+  payments:[],
+  lending: [],
+  byLetter: [],
+  byState: [],
+  byCity: [],
+  byRegion: [],
+  byYear: [],
+  activeInfo: [],
+  activeTab: 'US',
+  activeSegment: 'Country',
+  firstLoad: true,
+
+})
+
+export const mutations = {
+
+   setInsur(state, data) {
+
+    state.insurtech = data; 
+   	state.activeInfo = data;
+   	state.activeTab = 'Insurtech';
+    state.firstLoad = false;
+   
+   },
+
+   setBlock(state, data){
+
+  	state.blockchain = data;
+  	state.activeInfo = data;
+    state.activeTab = 'Blockchain';
+    state.firstLoad =false;
+    
+   },
+
+   setPay(state, data){
+
+   	 state.payments = data;
+     state.activeInfo = data;
+  	 state.activeTab = 'Payments';	
+  	 state.firstLoad =false;
+
+  	},
+
+   setLend(state, data){
+
+  	 state.lending = data;
+     state.activeInfo = data;
+     state.activTab = 'Lending';	
+     state.firstLoad =false;
+  	 
+   },
+
+   setBank(state, data) {
+
+   	 state.banking = data;
+     state.activeInfo = data;
+  	 state.activeTab = 'Banking';	
+  	 state.firstLoad =false;
+
+  	},
+	
+	setInsurNoFetch(state){
+
+	 state.activeInfo = state.insurtech;
+	 state.activeTab = 'Insurtech';	
+	 state.firstload = false;	
+	
+	 },
+
+	setBlockNoFetch(state) {
+
+	 state.activeInfo = state.blockchain;
+	 state.activeTab = 'Blockchain';	
+	 state.firstLoad =false;		
+	},
+
+	setPayNoFetch(state){
+		
+	  state.activeInfo = state.payments;
+	  state.activeTab = 'Payments';	
+	  state.firstLoad = false;
+	},
+
+	setLendNoFetch(state) {
+
+	  state.activeInfo = state.lending;
+      state.activeTab = 'Lending';	
+	  state.firstLoad = false;
+	},
+
+	setBankNoFetch(state) {
+
+	  state.activeInfo = state.banking;
+	  state.activeTab ='Banking';	
+	  state.firstLoad =false;
+	 
+	 },
+
+	 set(state, data) {
+
+		state.activeInfo = data;
+		state.US = data;
+		state.activeTab ='US';	
+		state.firstLoad =false;
+	 
+	 },
+	 
+ }
+
+ export const actions = {
+
+       	 async setInsur({ commit }) {
+
+			if (!this.insurFetched)
+			{
+				await this.$axios.get('/companies?category=insurtech')
+           		.then(res => {
+          			commit("setInsur", res.data);
+         		})
+			}
+
+			else
+			{
+				commit("setInsurNoFetch");
+			}	
+		},
+
+  		 async setBlock({ commit }) {
+
+		   if (!this.blockFetched)
+		   {
+  	       		await this.$axios.get('/companies?category=blockchain')
+           		.then(res => {
+          			commit("setBlock", res.data);
+         		})
+		   }
+		   else
+			{
+				commit("setBlockNoFetch");
+			}	
+		},
+
+  		 async setPay({ commit }) {
+
+			if (!this.payFetched)
+			{
+  	       		await this.$axios.get('/companies?category=payments')
+           		.then(res => {
+					  commit("setPay", res.data);
+				})
+			}	
+			
+			else	
+			{
+				commit("setPayNoFetch");
+			}	
+  		 
+  		 },
+
+  		 async setLend({ commit }) {
+
+		   if (!this.lendFetched)
+		   {	
+  	       		await this.$axios.get('/companies?category=lending')
+           			.then(res => {
+					  commit("setLend", res.data);
+		         })
+			}
+		   else
+
+		   {
+				commit("setLendNoFetch");
+		   }
+  		 
+  		 },
+
+  		 async setBank({ commit }) {
+		    
+		    if (!this.bankFetched)
+		   	{	
+  	    		await this.$axios.get('/companies?category=openbanking')
+           			.then(res => {
+          			commit("setBank", res.data);
+			 	})
+		   	} 
+		   	else {
+				
+				commit("setBankNoFetch" );
+		    }
+  		 
+  		  },
+
+  		  nuxtServerInit(vuexContext, context) {
+		   
+		   if (!this.firstFetched) { 
+
+              return this.$axios.$get("/companies?country=US")
+              .then(data => {
+                vuexContext.commit("set", data);
+              
+              })
+		   }
+
+		   else {
+			
+			  vuexContext.commit("set");
+		   }
+
+	      },   
+	      
+	 }  
+
+	export const getters = {
+      
+      loadedCompanies(state) {
+        
+        return state.company.US;
+	  
+	  },
+
+	  payFetched() {
+		
+		return state.company.payments.length > 0 ;
+	  
+	  },
+
+	  blockFetched() {
+		
+		return this.state.company.blockchain.length > 0 ;
+	  
+	  },
+	  
+	  bankFetched() {
+
+	  	return state.company.banking.length > 0 ;
+	 
+	  },
+
+	  insurFetched() {
+		 
+		 return state.company.insurtech.length > 0; 
+	  
+	  },
+	  
+	  lendFetched()  {
+	
+		return state.company.lending.length > 0;
+	  },
+	  
+	  firsFetched() {
+	
+		return state.company.US.length > 0;
+	  },
+
+      
+      firstPage() {
+	
+		return state.company.US ;
+	
+	  },
+	 
+	  insurtech() {
+	
+		return state.company.insurtech;
+	  },
+	 
+	  lending() {
+	
+		 return state.company.lending;
+	  },
+	  
+	  banking() {
+	
+		return state.company.banking;
+	 
+	  },	
+
+	}
