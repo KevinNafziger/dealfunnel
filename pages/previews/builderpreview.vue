6e14480549f1e72f24bf992d689586941c6ba1aa
abraham-felix <servicioscelfonica@gmail.com>
2021-01-06 11:56:49 -0700

    Builder preview

---
+++ pages/previews/builderpreview.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <center>
+    <div id="top" class="sectionix"><div class="title"><div  class="content"><br >
+
+      <h2 >Builder Preview<i style="font-size: 13px; text-align:right; margin-left:3px;">{{ filterMessage }}</i></h2>
+
+    </div></div></div>
+  </center>
+  <center>
+  <img class="builder-preview" src="https://filesforfintech.s3.amazonaws.com/images/giffs/Deal+Funnel+-+Builder+Preview.gif"> </img>
+  </center>
+</div>
+</template>
+<script>
+</script>
+<style>
+h2 {
+  margin-top:50px;
+}
+.builder-preview {
+  margin: 20px;
+}
+</style>
