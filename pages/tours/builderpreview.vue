cb056c177cb31fc8e33fbcf6dfc84653e8f9e066
abraham-felix <servicioscelfonica@gmail.com>
2021-01-14 15:22:26 -0700

    Tours | builder preview | css/html

--- pages/tours/builderpreview.vue
+++ pages/tours/builderpreview.vue
@@ -1,11 +1,15 @@
 <template>
 <div>
   <center>
-    <div id="top" class="sectionix"><div class="title"><div  class="content"><br >
-
-      <h2 >Builder Preview</h2> <nuxt-link to="/builder"><span class="mdi mdi-arrow-left"></span>Back</nuxt-link>
-
-    </div></div></div>
+    <div id="top" class="sectionix">
+      <div class="title">
+       <div  class="content"><br >
+          <h2 >Builder Preview</h2>
+          <nuxt-link to="/builder"><br>
+        <span class="mdi mdi-arrow-left"></span>Back</nuxt-link>
+      </div>
+    </div>
+  </div>
   </center>
   <center>
   <img class="builder-preview" src="https://filesforfintech.s3.amazonaws.com/images/giffs/Deal+Funnel+-+Builder+Preview.gif"> </img>
@@ -18,8 +22,12 @@
 h2 {
   margin-top:50px;
 }
+
 .builder-preview {
-  margin: 20px;
+box-shadow: 0px 0px 7px -5px;
+padding: 10px;
+margin-bottom: 100px;
+margin-top: 100px;
 }
 span.mdi.mdi-arrow-left {
     background: #4f81bd;
