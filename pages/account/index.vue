bd88ab07fe99bce05821442c7ee1415bf8b2f165
abraham-felix <apesyntax@gmail.com>
2021-02-15 14:06:27 -0700

    Pages | account btn | css

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,24 +1,49 @@
 
 <template>
- <div class="card">
-
-<div style="margin-right:auto; margin-left:auto;"
- 	class="mdi mdi-account-circle-outline mdi-24px text-block-20">	
- 	<nuxt-link to="/customer-portal">
-	<h2>Manage billing</h2></nuxt-link>
-</div>
-</div>
-
+  <body>
+    <br>
+    <div class="card">
+      <div class="center">
+ 	      <nuxt-link title="click to manage billing" class="nuxt-link mdi mdi-account-circle-outline mdi-24px gardient-button" to="/customer-portal">
+       	<br> Manage billing</nuxt-link>
+      </div>
+    </div>
+  </body>
 </template>
-
+<script>
+</script>
 <style scoped>
 
 .text-block-20 {
-  
+
       border-radius: 10px;
  }
-    
-  
+
+
+ .gardient-button {
+     display: grid;
+     background-image: linear-gradient(200deg, #4F81BD 10%, #4F81BD 39%, #8ab4f8 80%);
+     background-size: 400% 400% !important;
+     animation: gradient 15s ease infinite !important;
+     width: fit-content !important;
+     margin-left: auto !important;
+     margin-right: auto !important;
+     border-radius: 10px !important;
+     border: none !important;
+     padding: 10px 20px !important;
+     font-family: poppins !important;
+     color: #ffffff !important;
+     font-size: 12px !important;
+     transition: 0.3s;
+     margin-top: 20px;
+     margin-bottom: 20px;
+     font-size: 16px !important;
+     box-shadow: 0px 1px 10px -3px #474747 !important;
+ }
+
+ .gardient-button:hover {
+ box-shadow: 0px 1px 5px -3px #474747 !important;
+   }
 
 h2 {
     color: #4f81bd !important;
@@ -30,7 +55,15 @@ h2 {
     margin-bottom: 20px;
 }
 
-
+.nuxt-link {
+  text-decoration: none !important;
+  background:#4f81bd;
+}
+.nuxt-link:active {
+  text-decoration: none !important;
+  background:#4f71bd;
+  box-shadow: inset 0px 0px 10px -5px #2f2f2f !important;
+}
 .front-subscribe-button {
   color: white;
   font-weight: 400;
@@ -46,15 +79,19 @@ h2 {
 
 .card {
   color: white;
-  margin-top: 200px;
   max-width: 900px;
   margin-left: auto;
+  margin-top:200px;
   margin-right: auto;
   background: center;
+  height: 300px;
   border-radius: 10px;
   text-align: center;
 }
-
+.center {
+  align-items:center;
+  margin-top: 75px;
+}
 .imgdiv {
   margin: 20px;
 }
