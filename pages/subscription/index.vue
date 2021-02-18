0bb11471c3e8070d19e5069677211c6c42b1d6c0
abraham-felix <apesyntax@gmail.com>
2021-02-18 10:40:32 -0700

    Subscription | @click back to btn

--- pages/subscription/index.vue
+++ pages/subscription/index.vue
@@ -239,7 +239,7 @@ a {
          <li class="plan__feature-item"><span class="accent-2"></span></li>
           <hr>
        </ul>
-  <button class="pricing plan-item plan__cta-link" onclick="checkout" role="link">
+  <button class="pricing plan-item plan__cta-link" @click="checkout" role="link">
         Purchase
   </button>
      </article>
