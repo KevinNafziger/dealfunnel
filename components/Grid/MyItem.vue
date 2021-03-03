d78cc2537fd0302b66f366370e81c1bfad1cdb49
Kevin Nafziger <KevinNafziger@gmail.com>
2021-03-03 12:32:12 -0700

    MyItem

--- components/Grid/MyItem.vue
+++ components/Grid/MyItem.vue
@@ -144,7 +144,7 @@ style="display:flex;text-decoration:none;color:#14a0fd;">
     <img v-bind:src="dataList.find(item => item.id === 1169).logo_url">
     <h6> {{ dataList.find(item => item.id === 1169).title }} </h6>
     <hr>
-      <p> {{ dataList.find(item => item.id === 1169).interest.substring(0,227) + "..." }} </p>
+      <p> {{ dataList.find(item => item.id === 1169).interest.substring(0,207) + "..." }} </p>
  </div>
 </a>
 <!-- End of Block -->
