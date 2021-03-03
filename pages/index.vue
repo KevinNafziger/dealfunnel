39b8f348fd28b70268a924e82fc7dc0ebe7e30e1
Kevin Nafziger <KevinNafziger@gmail.com>
2021-03-03 01:41:31 -0700

    pages

--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,13 @@
   </h1><br>
   <p class="MsoNormal">
   <span class="intro-p">
-    <i>Fintech Media LatAm</i> features articles, reports, and data on fast growing companies in Latin America. While we primarily cover Fintech, our coverage has expanded to encompass technology businesses serving the financial services industry and beyond. This includes companies in Blockchain, DLT and Crypto as well as in HealthTech, Artificial Intelligence, LegalTech, and Cybersecurity.
+    <i>Fintech Media LatAm</i> features articles, reports, and data on fast growing companies in Latin America. While Fintech remains our focus, our coverage has expanded to encompass technology businesses serving the financial services industry and beyond. This includes companies in Blockchain, DLT and Crypto as well as in HealthTech, Artificial Intelligence, LegalTech, and Cybersecurity.
   </span>
   </p>
   <p class="intro-p" >
   <b>Exceptional</b> –
-  <span class="SpellE"><i>Fintech Media LatAm</i> </span>
-  is beloved by executives, dealmakers and practitioners including investment bankers, partners and associates of venture capital firms, industry attorneys, PE practitioners and economists. 
+  <span class="SpellE"><i>Fintech Media LatAm</i></span>
+  is beloved by executives, dealmakers and practitioners including investment bankers, partners and associates of venture capital firms, industry attorneys and PE practitioners. 
   </p>
   <p class="intro-p"><b>Data Driven</b>–<span class="SpellE"></span>
   <i>Fintech Media LatAm</i> members receive online access to searchable data on capital raises, companies and articles. The data can be exported to <i>Excel</i> and other spreadsheet programs.
@@ -30,12 +30,12 @@
   </p>
   <p class="intro-p">
   <b>Propietary Content</b>–<span class="SpellE"></span>
-  <i>Fintech Media LatAm</i> members receive online acccess to all FHM propietary content.</i>
-  This  includes reports on trending topics along with sets of auto-generated reports which include all of the company-related articles and capital raise data for that topic. <i>FHM's</i> auto-generated reports are grouped by strategic action, size of expected raise, timing of expected raise, geography, subsector, and by broader categories such as 'Blockchain', 'Insurtech', 'Payments', 'AI', 'Lending' and 'OpenBanking. <i>FHM</i> reports aggregate all of <i>FHM's</i> own company-specific intelligence on that topic, derived from interviews with an executive or executives at the featured company.
+  <i>Fintech Media LatAm</i> members receive online acccess to all of FHM;s propietary content.</i>
+  This includes reports on trending topics along with sets of auto-generated reports which include all of the company-related articles and capital raise data for that topic. <i>FHM's</i> auto-generated reports are grouped by strategic action, size of expected raise, timing of expected raise, geography, subsector, and by broader categories such as 'Blockchain', 'Insurtech', 'Payments', 'AI', 'Lending' and 'OpenBanking. <i>FHM</i> reports aggregate all of <i>FHM's</i> own company-specific intelligence on that topic, derived from interviews with an executive or executives at the featured company.
   Our reports can generate a "wow" from some readers when they see the sheer volume of company-specific intelligence they provide. <i>FHM </i> reports aren't designed to provide a quick overview but to serve practitioners who want to review a large amount of company-specific intelligence related to a topic in a single document. 
   </p>
   <p class="intro-p"><b>Value Proposition</b>–<span></span>
-  The Fintech Revolution is fueling greater economic growth across Latin America. <i>Fintech Media LatAm</i> showcases companies driving that growth. Additionally, <i>Fintech Media LatAm</i>  features propietary reports, data, and articles that serve as a rich repository of information to executives, practitioners, and dealmakers. 
+  The Fintech Revolution is fueling economic growth throughout Latin America. <i>Fintech Media LatAm</i> showcases the companies driving that growth. We also provides propietary reports, data, articles and newsletters that serve as a rich repository of information to executives, practitioners, and dealmakers. 
   </p>
   <nuxt-link class="gardient-button" to="/subscription">Plan Details</nuxt-link>
   <p class="intro-p"><b>Team</b>–<span></span>
